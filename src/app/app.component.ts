4869cab0d7411945bc9a208db59f288185fd8787
Maíra Tabata <ink.dheart@gmail.com>
2018-08-13 13:01:04 -0300

    feat: Adiciona inline css ao componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root', //referência para a tag na index.html
   templateUrl: './app.component.html', //um template SEMPRE deve ser apontado => selector e style não
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'], //diversos arquivos .css podem ser referenciados neste array
+  styles: [`
+    h1{
+      color: #f06;
+    }
+  `]
 })
 export class AppComponent {
   title = 'angular6-the-complete-guide';
