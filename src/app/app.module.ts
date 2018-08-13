49d1249ab0da769548c31de43d575a8d72d484b2
Maíra Tabata <ink.dheart@gmail.com>
2018-08-13 12:15:17 -0300

    feat: Declara novo componente para utilização

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component'; //Referencia o local que o novo componente estpa
 
-@NgModule({
+@NgModule({ //define quais funcionalidades o app deve conter e usar
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent //sem a declaração, é como se o método não existisse / como uma assinatura
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent] //serves as root component
+  bootstrap: [AppComponent] //serves as root component / avisa qual componente é o principal no start da aplicação
 })
 export class AppModule { }
