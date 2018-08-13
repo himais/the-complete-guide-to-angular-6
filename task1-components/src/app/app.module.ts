c9a491c7f452264f7da81fcddf8c6c7665a245b5
Maíra Tabata <ink.dheart@gmail.com>
2018-08-13 13:52:58 -0300

    feat: Adiciona o alerta de aviso

--- task1-components/src/app/app.module.ts
+++ task1-components/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
