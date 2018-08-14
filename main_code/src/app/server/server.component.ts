c3c1da31cc7dd87045719a5f4ad50a06cb903854
Maíra Tabata <ink.dheart@gmail.com>
2018-08-14 11:59:38 -0300

    S2A36: Adicionando classes dinamicamente com ngClass

--- main_code/src/app/server/server.component.ts
+++ main_code/src/app/server/server.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({ //decorator - anotação para a criação de um componente
     selector: 'app-server', //string que faz referência ao componente para chamada
-    templateUrl: './server.component.html' //html a ser utilizado pelo componente | precisa do caminho relativo
+    templateUrl: './server.component.html', //html a ser utilizado pelo componente | precisa do caminho relativo
+    styles: [`
+    .online{
+        color: white;
+    }`]
 })
 
 export class ServerComponent{
