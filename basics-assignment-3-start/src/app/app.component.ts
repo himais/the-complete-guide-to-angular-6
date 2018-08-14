b30adb38b469a2c8e428af5f6bbd56ff7f80af02
Maíra Tabata <ink.dheart@gmail.com>
2018-08-14 15:05:01 -0300

    add resolução do exercício 3

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  showSecretMessage: Boolean = false;
+  buttonClick: number = 0;
+  clicksLog: number[] = [];
+
+  onClickToDisplayMsg() {
+    this.showSecretMessage = !this.showSecretMessage;
+    this.buttonClick++;
+    this.clicksLog.push(Date.now());
+  }
 }
