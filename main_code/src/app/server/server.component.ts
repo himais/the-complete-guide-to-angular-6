a08e6f4cc9597073e3898e17bf93d182f816292a
Maíra Tabata <ink.dheart@gmail.com>
2018-08-14 11:46:03 -0300

    S2A35: Estilizando elementos dinamicamente (ngStyle)

--- main_code/src/app/server/server.component.ts
+++ main_code/src/app/server/server.component.ts
@@ -9,7 +9,15 @@ export class ServerComponent{
     serverId: number = 10; //or just serverId = 10; 
     serverStatus: string = 'offline';
 
-    getServerStatus(){
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+
+    getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
