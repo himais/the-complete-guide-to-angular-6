2355bfa1c96472d68326e073eab0da5e70f21676
Maíra Tabata <ink.dheart@gmail.com>
2018-08-15 15:40:36 -0300

    feat (S5A63): Adiciona alias para propriedade criada

--- cmp-databinding-start/src/app/server/server.component.ts
+++ cmp-databinding-start/src/app/server/server.component.ts
@@ -6,9 +6,11 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string}; // por padrão as propriedades são
+  @Input('serverElement') element: {type: string, name: string, content: string}; // por padrão as propriedades são
                                                                   // acessíveis apenas pelo seu próprio componente
                                                                   // @Input não limita a visibilidade
+                                                                  // entre parenteses coloca-se um alias para ser acessível 
+                                                                  // por outros componentes
 
   constructor() { }
 
