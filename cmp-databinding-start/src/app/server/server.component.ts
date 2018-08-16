51d3bb28683a7749da5d2e189cb7734abdfd31c8
Maíra Tabata <ink.dheart@gmail.com>
2018-08-16 00:20:58 -0300

    feat (S5A75): Acessa valor do ng-content
    * Usando referência local e @ContentChild

--- cmp-databinding-start/src/app/server/server.component.ts
+++ cmp-databinding-start/src/app/server/server.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, AfterContentInit, ContentChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css'],
-  encapsulation: ViewEncapsulation.Emulated /* define as opções para encapsulamento da view
+ /* encapsulation: ViewEncapsulation.Native define as opções para encapsulamento da view
                                             emulated: padrão / com encapsulamento
                                             none: remove qualquer encapsulamento
                                             native: mesmo comportamento do emulated, mas apenas em
                                             navegadores que suportam o >> shadow dom << */
 })
-export class ServerComponent implements OnInit {
+export class ServerComponent implements OnInit, AfterContentInit {
+  @ContentChild('paragraph') paragraph: ElementRef; //Pega valor do ng-content
   @Input('serverElement') element: {type: string, name: string, content: string}; // por padrão as propriedades são
                                                                   // acessíveis apenas pelo seu próprio componente
                                                                   // @Input não limita a visibilidade
@@ -20,6 +21,11 @@ export class ServerComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('on init / paragraph not ready: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentInit() {
+      console.log('AfterContentInit / paragraph ready: ' + this.paragraph.nativeElement.textContent);
   }
 
 }
