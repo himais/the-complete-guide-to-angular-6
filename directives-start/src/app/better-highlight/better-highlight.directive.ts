f5428534eb2a85d4a462a47fb79ab30ea7d8d63b
Maíra Tabata <ink.dheart@gmail.com>
2018-08-16 16:53:40 -0300

    feat (S7A88): Adiciona hostlistner para capturar eventos da diretiva

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,22 @@
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  /* Sempre usar o renderer para fazer mnaipulações do DOM 
+  /* Sempre usar o renderer para fazer mnaipulações do DOM
      All render methods: https://angular.io/api/core/Renderer2 */
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'aquamarine');
+  }
+
+  @HostListener('mouseenter') mouseover() { //can receive paramters (eventData: Event)
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'turquoise');
+  }
+
+  @HostListener('mouseleave') mouseleave() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'aquamarine');
   }
 
