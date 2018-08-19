62f10d3f824c3eff3f1065c45737aa1331e85977
Maíra Tabata <ink.dheart@gmail.com>
2018-08-19 03:23:14 -0300

    feat (S11A17): Adiciona rotas com links

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,6 +13,11 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'servers', component: ServersComponent }
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +31,8 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes) // informa as rotas para o app
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
