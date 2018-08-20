8e1dd7eab18d47449db7838ef237d28dafe06d4c
Maíra Tabata <ink.dheart@gmail.com>
2018-08-20 10:36:20 -0300

    feat (S11A132): add página not found

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -25,7 +26,11 @@ const appRoutes: Routes = [
       { path: ':id', component: ServerComponent },
       { path: ':id/edit', component: EditServerComponent }
     ]
-  }];
+  },
+  { path: 'not-found', component: NotFoundComponent },
+  { path: '**', redirectTo: '/not-found' } /* pega todos os path não mapeados e redirecionam pra not-found
+                                              deve ser colocado nofinal pois as rotas são carregadas por ordem de declaração*/
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -34,7 +39,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
