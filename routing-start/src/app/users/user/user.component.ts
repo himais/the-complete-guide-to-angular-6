20b3eb3cce09638909e61a23877348f0b61281dc
Maíra Tabata <ink.dheart@gmail.com>
2018-08-20 00:38:52 -0300

    feat (S11A125): unsubscribe do route params on destroy

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number, name: string };
+  paramsSubscribe: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -27,7 +29,12 @@ export class UserComponent implements OnInit {
           this.user.id = params['id'];
           this.user.name = params['name'];
         }
-      )
+      );
+  }
+
+  ngOnDestroy() {
+    /* - O Angular não remove o subscribe ao destruir o componente - é necessário fazê-lo manualmente */
+    this.paramsSubscribe.unsubscribe();
   }
 
 }
